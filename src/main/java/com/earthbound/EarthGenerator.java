555ce1e7622f4a641c1f1d8873ce36abe2278c5e
mattwold41 <mattwold41@gmail.com>
2026-09-24 12:35:19 -0500

    Update EarthGenerator.java

--- src/main/java/com/earthbound/EarthGenerator.java
+++ src/main/java/com/earthbound/EarthGenerator.java
@@ -5,7 +5,7 @@ import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.event.aworld.ChunkLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EarthGenerator implements Listener {
