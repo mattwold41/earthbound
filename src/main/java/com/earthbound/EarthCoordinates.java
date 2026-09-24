20e6180bfb0a32956a37306899c0da9c3f3dd0b9
mattwold41 <mattwold41@gmail.com>
2026-09-24 14:38:27 -0500

    Update EarthCoordinates.java

--- src/main/java/com/earthbound/EarthCoordinates.java
+++ src/main/java/com/earthbound/EarthCoordinates.java
@@ -2,19 +2,31 @@ package com.earthbound;
 
 public class EarthCoordinates {
 
-    // Guemes Island reference point
-    private static final double START_LAT = 48.5500;
-    private static final double START_LON = -122.5800;
+    // Real world anchor point
+    // Guemes Island Ferry Terminal
+    private static final double START_LAT = 48.5228603;
+    private static final double START_LON = -122.624694;
 
-    // Minecraft scale
+
+    // Minecraft location of the anchor
+    private static final int START_X = -789;
+    private static final int START_Z = -501;
+
+
+    // EarthBound scale
     // 1 block = 2 meters
     private static final double METERS_PER_BLOCK = 2.0;
 
+
     public static double getLatitude(int x, int z) {
 
-        double metersNorth = -z * METERS_PER_BLOCK;
+        double metersNorth =
+                (START_Z - z) * METERS_PER_BLOCK;
 
-        double latitudeChange = metersNorth / 111320.0;
+
+        double latitudeChange =
+                metersNorth / 111320.0;
+
 
         return START_LAT + latitudeChange;
     }
@@ -22,11 +34,15 @@ public class EarthCoordinates {
 
     public static double getLongitude(int x, int z) {
 
-        double metersEast = x * METERS_PER_BLOCK;
+        double metersEast =
+                (x - START_X) * METERS_PER_BLOCK;
+
 
         double longitudeChange =
                 metersEast /
-                (111320.0 * Math.cos(Math.toRadians(START_LAT)));
+                (111320.0 *
+                Math.cos(Math.toRadians(START_LAT)));
+
 
         return START_LON + longitudeChange;
     }
