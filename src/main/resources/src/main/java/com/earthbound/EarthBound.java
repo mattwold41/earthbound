39d32dec3bcecdc990b9bdbdc7ca9133e63cb984
mattwold41 <mattwold41@gmail.com>
2026-09-23 20:09:40 -0500

    Update EarthBound.java

--- src/main/resources/src/main/java/com/earthbound/EarthBound.java
+++ src/main/resources/src/main/java/com/earthbound/EarthBound.java
@@ -9,20 +9,18 @@ public class EarthBound extends JavaPlugin {
     public void onEnable() {
         getLogger().info("EarthBound is now online!");
 
-        this.registerCommand("earth", (sender, args) -> {
+        registerCommand("earth", (sender, args) -> {
 
             if (args.length == 0) {
-                sender.sendPlainMessage("EarthBound - Real Earth Simulation");
-                sender.sendPlainMessage("/earth locate - Show your EarthBound location");
+                sender.sendMessage("EarthBound - Real Earth Simulation");
+                sender.sendMessage("/earth locate - Show your EarthBound location");
                 return;
             }
 
             if (args[0].equalsIgnoreCase("locate")) {
 
                 if (!(sender instanceof Player player)) {
-                    sender.sendPlainMessage(
-                            "This command can only be used by a player."
-                    );
+                    sender.sendMessage("This command can only be used by a player.");
                     return;
                 }
 
@@ -30,18 +28,18 @@ public class EarthBound extends JavaPlugin {
                 double y = player.getLocation().getY();
                 double z = player.getLocation().getZ();
 
-                player.sendPlainMessage("EARTHBOUND");
-                player.sendPlainMessage("Minecraft Coordinates:");
-                player.sendPlainMessage("X: " + String.format("%.2f", x));
-                player.sendPlainMessage("Y: " + String.format("%.2f", y));
-                player.sendPlainMessage("Z: " + String.format("%.2f", z));
-                player.sendPlainMessage("Earth coordinates: Coming soon");
-                player.sendPlainMessage("Real-world location: Coming soon");
+                player.sendMessage("EARTHBOUND");
+                player.sendMessage("Minecraft Coordinates:");
+                player.sendMessage("X: " + String.format("%.2f", x));
+                player.sendMessage("Y: " + String.format("%.2f", y));
+                player.sendMessage("Z: " + String.format("%.2f", z));
+                player.sendMessage("Earth coordinates: Coming soon");
+                player.sendMessage("Real-world location: Coming soon");
 
                 return;
             }
 
-            sender.sendPlainMessage("Unknown EarthBound command.");
+            sender.sendMessage("Unknown EarthBound command.");
         });
     }
 }
