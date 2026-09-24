991a6eaf526d6e468cbb37b356e6a7cebdbde0ad
mattwold41 <mattwold41@gmail.com>
2026-09-24 00:55:58 -0500

    Create EarthSpawn.java

---
+++ src/main/java/com/earthbound/EarthSpawn.java
@@ -0,0 +1,39 @@
+package com.earthbound;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EarthSpawn implements Listener {
+
+    private final JavaPlugin plugin;
+
+    public EarthSpawn(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+
+        Player player = event.getPlayer();
+
+        World world = player.getWorld();
+
+        // Temporary Guemes Island spawn
+        Location guemesSpawn = new Location(
+                world,
+                0,
+                80,
+                0
+        );
+
+        player.teleport(guemesSpawn);
+
+        player.sendMessage(
+                "§6EarthBound §7spawned you at Guemes Island test area."
+        );
+    }
