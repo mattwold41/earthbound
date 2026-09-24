6f3f1c5c0c1624f6a105b574baefa5fdc18af6b1
mattwold41 <mattwold41@gmail.com>
2026-09-24 15:15:23 -0500

    Update EarthLocation.java

--- src/main/java/com/earthbound/EarthLocation.java
+++ src/main/java/com/earthbound/EarthLocation.java
@@ -2,26 +2,54 @@ package com.earthbound;
 
 public class EarthLocation {
 
-    private static final double GUEMES_LAT = 48.56;
-    private static final double GUEMES_LON = -122.59;
+    // Guemes Island Ferry Terminal reference point
+    private static final double GUEMES_LAT = 48.5228603;
+    private static final double GUEMES_LON = -122.624694;
 
-    // Test radius around Guemes Island
+
+    // Search radius around Guemes Island
     private static final double TEST_RADIUS_KM = 20.0;
 
-    public static boolean isNearGuemes(double latitude, double longitude) {
 
-        double latDistance = latitude - GUEMES_LAT;
-        double lonDistance = longitude - GUEMES_LON;
+    public static boolean isNearGuemes(
+            double latitude,
+            double longitude) {
+
+
+        double latDistance =
+                latitude - GUEMES_LAT;
+
+        double lonDistance =
+                longitude - GUEMES_LON;
+
 
         double distance =
                 Math.sqrt(
-                    (latDistance * latDistance) +
-                    (lonDistance * lonDistance)
+                        (latDistance * latDistance) +
+                        (lonDistance * lonDistance)
                 );
 
-        // Rough conversion to kilometers
-        double kilometers = distance * 111;
+
+        // Convert rough degree distance to kilometers
+        double kilometers =
+                distance * 111;
+
 
         return kilometers <= TEST_RADIUS_KM;
+    }
+
+
+    public static String getRegion(
+            double latitude,
+            double longitude) {
+
+
+        if (isNearGuemes(latitude, longitude)) {
+
+            return "Guemes Island, Washington";
+        }
+
+
+        return "Unknown EarthBound Region";
     }
 }
