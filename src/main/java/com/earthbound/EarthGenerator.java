c2f30cdb4cadc6d28259f1530242def281d1348f
mattwold41 <mattwold41@gmail.com>
2026-09-24 00:42:46 -0500

    Create EarthGenerator.java

---
+++ src/main/java/com/earthbound/EarthGenerator.java
@@ -0,0 +1,48 @@
+package com.earthbound;
+
+import org.bukkit.Material;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EarthGenerator implements Listener {
+
+    private final JavaPlugin plugin;
+
+    public EarthGenerator(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onChunkLoad(ChunkLoadEvent event) {
+
+        Chunk chunk = event.getChunk();
+
+        int x = chunk.getX() * 16;
+        int z = chunk.getZ() * 16;
+
+        double latitude =
+                EarthCoordinates.getLatitude(x, z);
+
+        double longitude =
+                EarthCoordinates.getLongitude(x, z);
+
+        if (EarthLocation.isNearGuemes(latitude, longitude)) {
+
+            generateGuemesTest(chunk);
+        }
+    }
+
+
+    private void generateGuemesTest(Chunk chunk) {
+
+        Location location =
+                chunk.getBlock(8, 64, 8).getLocation();
+
+        location.getBlock()
+                .setType(Material.GRASS_BLOCK);
+    }
+}
