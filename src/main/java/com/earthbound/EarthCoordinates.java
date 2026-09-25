acd7be352c23d98207d62ec1db59758fa4608d2f
mattwold41 <mattwold41@gmail.com>
2026-09-24 21:18:19 -0500

    Update EarthCoordinates.java

--- src/main/java/com/earthbound/EarthCoordinates.java
+++ src/main/java/com/earthbound/EarthCoordinates.java
@@ -2,68 +2,99 @@ package com.earthbound;
 
 public class EarthCoordinates {
 
-
     /*
-     * EarthBound scale:
+     * EarthBound geographic anchor.
      *
-     * 1 Minecraft block = 2 real-world meters
+     * Guemes Island Ferry Terminal
+     * Minecraft: X -624, Z -544
+     * Earth: 48.5228603, -122.624694
      */
-    private static final double METERS_PER_BLOCK = 2.0;
 
+    private static final double START_LAT =
+            48.5228603;
+
+    private static final double START_LON =
+            -122.624694;
+
+    private static final int START_X =
+            -624;
+
+    private static final int START_Z =
+            -544;
 
     /*
-     * Guemes Island starting point
+     * EarthBound horizontal scale:
+     * 1 Minecraft block = 2 real meters.
      */
-    private static final double SPAWN_LAT =
-            48.5265;
+    private static final double METERS_PER_BLOCK =
+            2.0;
 
-    private static final double SPAWN_LON =
-            -122.6165;
 
+    private EarthCoordinates() {
+    }
 
 
     public static double getLatitude(
             int x,
             int z) {
 
-        return minecraftToLatitude(z);
-    }
+        double metersNorth =
+                (START_Z - z)
+                        * METERS_PER_BLOCK;
 
+        double latitudeChange =
+                metersNorth
+                        / 111320.0;
+
+        return START_LAT
+                + latitudeChange;
+    }
 
 
     public static double getLongitude(
             int x,
             int z) {
 
-        return minecraftToLongitude(x);
+        double metersEast =
+                (x - START_X)
+                        * METERS_PER_BLOCK;
+
+        double longitudeChange =
+                metersEast
+                        /
+                        (111320.0
+                                * Math.cos(
+                                        Math.toRadians(
+                                                START_LAT
+                                        )
+                                ));
+
+        return START_LON
+                + longitudeChange;
     }
 
 
+    /*
+     * Compatibility methods used by
+     * EarthLocation.java.
+     */
 
     public static double minecraftToLatitude(
             int z) {
 
-        return SPAWN_LAT -
-                (z * METERS_PER_BLOCK
-                        / 111320.0);
+        return getLatitude(
+                START_X,
+                z
+        );
     }
-
 
 
     public static double minecraftToLongitude(
             int x) {
 
-        double metersPerLongitude =
-                111320.0 *
-                        Math.cos(
-                                Math.toRadians(
-                                        SPAWN_LAT
-                                )
-                        );
-
-
-        return SPAWN_LON +
-                (x * METERS_PER_BLOCK
-                        / metersPerLongitude);
+        return getLongitude(
+                x,
+                START_Z
+        );
     }
 }
